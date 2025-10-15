e3a7a8588498e023c7e043c8fb999e2a52f7fc02
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-10-15 09:55:30 +0900

    Feat/버튼 컴포넌트 개발 (#7)
    * chore: 스토리 스닙펫 수정
    
    * design: 전역 스타일 네이밍 수정
    
    * feat: 버튼 컴포넌트 개발
    
    * feat: 버튼 컴포넌트 개발
    
    * test: 버튼 테스트 작성

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,45 @@
+import { ReactNode, ButtonHTMLAttributes } from 'react';
+
+import { cn } from '@/utils/cn';
+
+type ButtonVariant = 'primary' | 'secondary' | 'tertiary' | 'disabled';
+type ButtonSize = 'small' | 'medium' | 'large';
+
+const variantClasses = {
+  primary: 'bg-primary hover:bg-purple-100 hover:!text-purple-500',
+  secondary: 'bg-purple-700 hover:!bg-purple-200 hover:!text-purple-500',
+  tertiary: 'bg-purple-800 hover:!bg-purple-300 hover:!text-purple-500',
+  disabled: 'bg-gray-300 hover:!bg-gray-100 hover:!text-gray-400',
+} as const satisfies Record<ButtonVariant, string>;
+
+const sizeClasses = {
+  small: 'h-[56px] rounded-[16px] ds-typo-body1-bold',
+  medium: 'h-[50px]  rounded-[12px] ds-typo-body2-bold',
+  large: 'h-[44px] rounded-[12px]',
+} as const satisfies Record<ButtonSize, string>;
+
+const baseStyle = 'flex items-center justify-center cursor-pointer px-4 text-white';
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  variant?: ButtonVariant;
+  size?: ButtonSize;
+  className?: string;
+  disabled?: boolean;
+  onClick?: () => void;
+}
+
+const Button = ({ children, variant = 'primary', size = 'medium', className, disabled = false, onClick, ...rest }: ButtonProps) => {
+  return (
+    <button
+      {...rest}
+      className={cn(baseStyle, variantClasses[variant], sizeClasses[size], className)}
+      disabled={disabled}
+      onClick={onClick ? () => onClick() : () => {}}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
