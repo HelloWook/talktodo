2eb0bb24a469a8f3fe28551c5d558f056b2f73e0
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-10-23 22:51:09 +0900

    feat: 마이페이지 구조 설정

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,16 +1,9 @@
-'use client';
-import UserForm from '@/components/UserForm/UserForm';
+import MyPageForm from '@/components/MyPageForm/MyPageForm';
 
 const MyPage = () => {
   return (
-    <div className='flex h-screen items-center justify-center'>
-      <UserForm>
-        <UserForm.Title title='마이 페이지' />
-        <UserForm.ProfileUpload />
-        <UserForm.FieldGroup fieldName='이름' />
-        <UserForm.FieldGroup fieldName='이메일' />
-        <UserForm.FormButton className='w-full'>로그인</UserForm.FormButton>
-      </UserForm>
+    <div className='gradient-bg flex h-screen items-center justify-center'>
+      <MyPageForm />
     </div>
   );
 };
