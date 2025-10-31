b9bb9561a26262ad38464d170228cc8a6afe51fa
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-10-31 10:13:24 +0900

    design: 포맷팅 적용

--- src/components/DatePicker/Caption/Caption.tsx
+++ src/components/DatePicker/Caption/Caption.tsx
@@ -6,7 +6,7 @@ const Caption = () => {
   const { months } = useDayPicker();
 
   return (
-    <span className='flex items-center w-[200px] ml-[8px]' role='status' aria-live='polite'>
+    <span className='ml-[8px] flex w-[200px] items-center' role='status' aria-live='polite'>
       {months[0].date.getFullYear()}년 {months[0].date.getMonth() + 1} 월
     </span>
   );
