ec14095d176f054024c03667c8c955a8bd729e8c
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-11-17 22:02:35 +0900

    env: 스토리북 cicd 구축

--- src/components/MyPageForm/MyPage.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-
-import MyPageForm from './MyPageForm';
-
-describe('MyPageForm', () => {
-  it('마이 페이지 폼이 렌더링 된다.', () => {
-    render(<MyPageForm />);
-  });
-});
